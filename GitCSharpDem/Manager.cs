e061dbfe0d54abd74440fbdfb3ad247b14bcddce
Panos Dallas <dallaspanos@gmail.com>
2022-05-07 12:09:02 +0300

    Added Manager and Developer Entities

---
+++ GitCSharpDem/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCSharpDem
+{
+    class Manager : Person
+    {
+        List<Developer> Developers { get; set; }
+
+    }
+}
